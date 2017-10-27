6dc15cea3db17158b23c98e42408f5668eb7c7e1
ManasesZabala <manaseszabala@hotmail.com>
2017-10-27 20:54:33 -0300

    Add files via upload

---
+++ Zalaba/Profesor/PRACTICA TP4/Practica TP4 Mana/parser.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+
+int parserEmployee(FILE* pFile , ArrayList* pArrayListEmployee)
+{
+    char auxNombre[51];
+    char auxApellido[51];
+    char auxId[5];
+    char auxEmpty[6];
+    if(pFile!=NULL && pArrayListEmployee!=NULL)
+    {
+        fscanf(pFile,"%[^,],%[^,],%[^,],%[^,],%s",auxId,auxNombre,auxApellido,auxEmpty);
+        /*Indico con "[^,]" que lea hasta que encuentre una coma y luego me lo guarde en el respectivo auxiliar.
+          la coma entre cada "[^,]" indica que se saltee dicha coma del archivo que esté leyendo, ya que no necesito
+          guardar esa coma en mi array list.
+        */
+
+
+
+
+
+
+    }
+    else
+    {
+        printf("\nNo se pudo abrir el archivo o el array");
+        return 1;
+    }
+
+    return 0;
+}
